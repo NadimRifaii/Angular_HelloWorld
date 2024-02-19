188fd7dfae0c05dd4c6077e9741e26a0b742d184
NadimRifaii <nadimrifaii3@gmail.com>
2024-02-19 18:22:51 +0200

    Angular events

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -16,8 +16,15 @@ export class AppComponent {
   title = 'wishlist';
   items: WishtItem[] = [
     new WishtItem('To Learn Angular', false),
-    new WishtItem('To Learn React', false),
+    new WishtItem('To Learn React', true),
     new WishtItem('To Learn Vue', false)
   ];
-
+  newWishText = ''
+  addWish() {
+    this.items.push(new WishtItem(this.newWishText, false))
+    this.newWishText = ''
+  }
+  toggleItem(e: any, item: WishtItem) {
+    item.isCompleted = !item.isCompleted;
+  }
 }
--- wishlist/src/app/app.module.ts
+++ wishlist/src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+/**
+ * The AppComponent belongs to this module (NgModule) because it's declared in it
+ * 
+ * The imports are things that are available to the templates (html files) inside of the module (NgModule)
+ * 
+ * The providers are objects that can be injected to parts of this module 
+ * 
+ * bootstrap is what is bootstrapped, it is the entry point of the module which is in our case the AppComponent
+ */
