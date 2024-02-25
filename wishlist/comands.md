33dcdc2070aefa1c8a70d4153bde32f7f4fabc8b
NadimRifaii <nadimrifaii3@gmail.com>
2024-02-25 17:24:27 +0200

    Creating modules

--- wishlist/comands.md
+++ wishlist/comands.md
@@ -1,7 +1,7 @@
 ng serve
 ng generate component component-name
 ng generate service service-name
-
+ng generate module module-name
 /*
   
 */
--- wishlist/src/app/app.module.ts
+++ wishlist/src/app/app.module.ts
@@ -1,29 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { WishListComponent } from './wish-list/wish-list.component';
-import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
-import { WishFilterComponent } from './wish-filter/wish-filter.component';
-import { WishListItemComponent } from './wish-list-item/wish-list-item.component';
+
 
 import { EventService } from 'src/shared/services/EventService';
+import { WishModule } from './wish/wish.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WishListComponent,
-    AddWishFormComponent,
-    WishFilterComponent,
-    WishListItemComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,  // to have access to the ngModel
-    HttpClientModule,//We make the HttpClientModule available throughout our entire application
+    WishModule
   ],
   providers: [
     EventService,
