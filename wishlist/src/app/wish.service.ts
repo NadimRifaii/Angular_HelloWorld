25870885b6888fa361bf610d56ad52f807395578
NadimRifaii <nadimrifaii3@gmail.com>
2024-02-25 14:00:55 +0200

    Providing options for HTTP requests

--- wishlist/src/app/wish.service.ts
+++ wishlist/src/app/wish.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { WishItem } from 'src/shared/models/wishItem';
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +8,33 @@ import { HttpClient } from '@angular/common/http';
 export class WishService {
 
   constructor(private http: HttpClient) { }
+  private getStandardOptions(): any {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+  }
   getWishes() {
-    return this.http.get(`assets/wishes.json`)
+    const options = this.getStandardOptions()
+    options.params = new HttpParams({
+      fromObject: {
+        format: 'json'
+      }
+    })
+    return this.http.get(`assets/wishes.json`, options)
+    return this.http.get(`assets/wishes.json?format=json`, options)
     /**
      * The get method returns an observable 
      * By calling the get method , the actual request has not been sent yet 
      * Since this is an observable object, there is a subscribe method that we can use
      * When we call the subscribe method , then the request is sent 
-     */
+    */
+  }
+  private addWish(wish: WishItem) {
+    const options = this.getStandardOptions()
+    options.headers = options.headers.set(`Authorization`, 'value')
+    this.http.post(`url`, wish, options)
+    //url, body, options
   }
 }
