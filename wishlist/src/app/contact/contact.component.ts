4da35e03b2861b7f0e424fe005d113a00e214075
NadimRifaii <nadimrifaii3@gmail.com>
2024-02-25 18:41:08 +0200

    Validating reactive forms

--- wishlist/src/app/contact/contact.component.ts
+++ wishlist/src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -10,9 +10,9 @@ export class ContactComponent {
   // senderEmailControl = new FormControl(''),
   // senderMessageControl = new FormControl(''),
   contactForm = new FormGroup({
-    senderName: new FormControl(''),
-    senderEmail: new FormControl(''),
-    senderMessage: new FormControl(''),
+    senderName: new FormControl('', [Validators.required,]),
+    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderMessage: new FormControl('', [Validators.required, Validators.minLength(10)]),
   })
   submitForm() {
     console.log(this.contactForm.value)
