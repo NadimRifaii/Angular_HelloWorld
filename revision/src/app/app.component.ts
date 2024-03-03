f7e60d81dc6f09110257b73b0782676d9d3cf6ef
NadimRifaii <nadimrifaii3@gmail.com>
2024-03-03 18:35:15 +0200

    Revised the ngModel

--- revision/src/app/app.component.ts
+++ revision/src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/models/WishItem';
 
+/**
+ * The component decorator gives us information about the component
+ */
+const filters = [
+  (item: WishItem) => item,
+  (item: WishItem) => !item.isComplete,
+  (item: WishItem) => item.isComplete,
+]
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items: WishItem[] = [
+    new WishItem('To learn angular', true),
+    new WishItem('To revise angular'),
+    new WishItem("To master angular")
+  ];
+  listFilter: string = '0'
+  newWishText: string = ''
   title = 'revision';
+  // visibleItems: WishItem[] = this.items
+  addWishItem() {
+    this.items.push(new WishItem(this.newWishText));
+    // this.filterChange(this.listFilter)
+    this.newWishText = '';
+  }
+
+  filterChange(value: any) {
+    // if (value == '0')
+    //   this.visibleItems = this.items
+    // if (value == '1')
+    //   this.visibleItems = this.items.filter(item => !item.isComplete)
+    // if (value == '2')
+    //   this.visibleItems = this.items.filter(item => item.isComplete)
+  }
+  /**
+   * The getter allows us to react to any change that we make to the original items array
+   * Any time the items array is modified or the objects inside of it are modified is going to automatically be reflected within the browser
+   */
+  get visibleItems(): WishItem[] {
+    return this.items.filter(filters[+this.listFilter])
+  }
+  toggleItem(event: Event, item: WishItem) {
+    item.isComplete = !item.isComplete;
+    // this.filterChange(this.listFilter)
+  }
 }
--- revision/src/app/app.module.ts
+++ revision/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+/**
+ * This file defines practically everything that our application requires in order to run
+ * 
+ * NgModule:{
+ * declarations:Set of components that belongs to this module
+ * 
+ * imports:Things that are available to the templates inside this module
+ * 
+ * providers: objects that can be injected to parts of this module
+ * 
+ * bootstrap: The entry point of our module
+ * }
+ */
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule//to use the ngModel
   ],
   providers: [],
   bootstrap: [AppComponent]
